37152237028ef112777659d4f335f4ef1ed27b48
shivsh94 <shivamsharma23jan@gmail.com>
2025-02-23 15:48:35 +0530

    Remove commented-out greeting endpoint from the backend

--- backend/index.js
+++ backend/index.js
@@ -18,9 +18,9 @@ const io = new Server(server, {
 });
 
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+// app.get("/", (req, res) => {
+//   res.send("hello");
+// });
 
 const emailToSocketMap = new Map();
 const socketidToEmailMap = new Map();
